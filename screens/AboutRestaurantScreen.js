8c307c7ec82ec18452e7abdbf28c1e0046b1dcb9
Szymon Tyrka <szy.tyrka@gmail.com>
2021-04-29 20:00:37 +0200

    create empty necessary file

---
+++ screens/AboutRestaurantScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+
+export class AboutRestaurantScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{ justifyContent: "center", alignItems: "center" }}>O restauracji</Text>
+            </View>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#f2f2f4",
+        flex: 1,
+    }
+});
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+
+export class RegisterScreen extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{ justifyContent: "center", alignItems: "center" }}>Rejestracja</Text>
+            </View>
+        );
+    }
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#f2f2f4",
+        flex: 1,
+    }
+});
