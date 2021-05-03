af60a506368ee4d767089aa62ef67c860c192801
razzin <mastertogo@vp.pl>
2021-05-04 01:54:07 +0200

    about-restaurant screen

--- screens/AboutRestaurantScreen.js
+++ screens/AboutRestaurantScreen.js
@@ -1,18 +1,64 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ImageBackground} from "react-native";
 import Header from "../components/Header";
 
+const RESTAURANT_NAME="\"Na Dziedzińcu\"";
+const ADDRESS="Lwowska 4,33-100 Tarnów";
+const MAIL="testRestauracja@gmail.com";
+const PHONE="+48146960912";
+const DESCRIPTION="Prawdziwa restauracja z wieloletnią tradycją, sięgającą 1926r.\n"
+				 +"Nasze dania wykonujemy z najwyższej jakości składników, aby nasi klienci mogli delektować się smakiem "
+				 +"tradycyjnej polskiej kuchni.";
 export class AboutRestaurantScreen extends Component {
 
     constructor(props) {
         super(props);
         
     }
+	
+	onLoad = (event) => {
+  // log a message showing the map has been loaded
+  console.log('onLoad received : ', event);
+
+  // optionally set state
+  this.setState(
+    {
+      ...this.state,
+      mapState: { ...this.state.mapState, mapLoaded: true }
+    },
+    () => {
+      // send an array of map layer information to the map
+      this.webViewLeaflet.sendMessage({
+        mapLayers
+      });
+    }
+  );
+}
+	
 
     render() {
         return (
             <View style={styles.container}>
-                 <Header navigation={this.props.navigation} title="O restauracji" />
+                <Header navigation={this.props.navigation} title="O restauracji" />
+				<View style={styles.leaflet}>
+					<ImageBackground source={require('../images/backgraund-image.jpg')} style={styles.image}>
+						<View style={styles.leafletShadow}>
+							<View style={styles.leafletTop}>
+								<Text style={[styles.textNormal, styles.textColorPrimary]}>Restauracja</Text>
+								<Text style={[styles.textBolded, styles.textColorSecondary]}>{RESTAURANT_NAME}</Text>
+							</View>
+							<Text style={[styles.textContent, styles.textColorPrimary, styles.textBolded]}>Kontakt</Text>
+							<Text style={[styles.textContent, styles.textColorSecondary]}>{ADDRESS}</Text>
+							<Text style={[styles.textContent, styles.textColorSecondary]}>{MAIL}</Text>
+							<Text style={[styles.textContent, styles.textColorSecondary]}>{PHONE}</Text>
+							<Text style={[styles.textContent, styles.textColorPrimary]}>{DESCRIPTION}</Text>
+						</View>
+					</ImageBackground>
+				</View>
+				
+				<View style={styles.map}>
+					<ImageBackground source={require('../images/map.jpg')} style={styles.image}></ImageBackground>
+				</View>
             </View>
         );
     }
@@ -21,5 +67,44 @@ export class AboutRestaurantScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    }
+    },
+	image: {
+        flex: 1,
+        resizeMode: "cover",
+    },
+	leaflet: {
+		height: 400,
+	},
+	leafletShadow: {
+		height:400,
+		backgroundColor: "rgba(25,20,19,0.8)",
+	},
+	leafletTop: {
+		marginTop:12,
+		marginBottom:32,
+		alignItems: "center",
+	},
+	textNormal: {
+		fontSize: 18,
+	},
+	textBolded: {
+		fontSize: 25,
+	},
+	textColorPrimary: {
+		color: 'white'
+	},
+	textColorSecondary: {
+		color: 'darkorange'
+	},
+	textContent: {
+		fontSize: 17,
+		marginLeft: 32,
+		marginRight: 32,
+		margin: 4
+	},
+	map: {
+		height:264,
+		width:393,
+		backgroundColor: 'red'
+	}
 });
